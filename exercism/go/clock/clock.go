dbbf4a2db4897fde2161feb7718fc0315b6cff37
Sergei Eremeev <prounckk@gmail.com>
2022-07-05 06:22:33 -0400

    rust - lucians-luscious-lasagna

---
+++ exercism/go/clock/clock.go
@@ -0,0 +1,25 @@
+package clock
+
+import "fmt"
+// Define the Clock type here.
+
+func New(h, m int) Clock {
+	panic("Please implement the New function")
+}
+
+func (c Clock) Add(m int) Clock {
+	panic("Please implement the Add function")
+}
+
+func (c Clock) Subtract(m int) Clock {
+	panic("Please implement the Subtract function")
+}
+
+func (c Clock) String() string {
+	panic("Please implement the String function")
+}
+
+
+// nicola -> postman byt still blocked
+// pr updated 
+// 
