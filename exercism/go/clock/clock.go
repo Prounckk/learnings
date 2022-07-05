c4bc8c200ea4208e90454153908916678325c613
Sergei Eremeev <prounckk@gmail.com>
2022-07-05 18:10:09 -0400

    golang - clock

--- exercism/go/clock/clock.go
+++ exercism/go/clock/clock.go
@@ -1,25 +1,46 @@
 package clock
 
 import "fmt"
-// Define the Clock type here.
+
+type Clock struct{ h, m int }
 
 func New(h, m int) Clock {
-	panic("Please implement the New function")
+	c := Clock{h, m}
+	return c.normilizer()
 }
 
 func (c Clock) Add(m int) Clock {
-	panic("Please implement the Add function")
+	c.m += m
+	return c.normilizer()
 }
 
 func (c Clock) Subtract(m int) Clock {
-	panic("Please implement the Subtract function")
+	c.m -= m
+	return c.normilizer()
 }
 
 func (c Clock) String() string {
-	panic("Please implement the String function")
+	return fmt.Sprintf("%02d:%02d", c.h, c.m)
 }
 
+func (c Clock) normilizer() Clock {
+	if c.m >= 60 {
+		c.h += c.m / 60 // hours
+		c.m %= 60       // minutes
+	}
+	if c.m < 0 {
+		c.h--
+		c.m += 60
+	}
+	if c.h >= 24 { // hours
+		c.h %= 24 // hours
+	}
+	if c.h < 0 { // hours	// negative hours
+		c.h = 24 + c.h // hours
+	}
+	if c.h < 0 || c.m > 60 || c.m < 0 {
+		return c.normilizer()
+	}
 
-// nicola -> postman byt still blocked
-// pr updated 
-// 
+	return c
+}
